062503425d068c49c58355404942f929a4a86fad
allan zizinga <allanzizinga@yahoo.com>
2024-04-19 11:21:25 +0200

    calculate surplus data

--- run.py
+++ run.py
@@ -1,5 +1,6 @@
 import gspread
 from google.oauth2.service_account import Credentials
+from pprint import pprint
 
 SCOPE = [
     "https://www.googleapis.com/auth/spreadsheets",
@@ -27,11 +28,11 @@ def get_sales_data():
         print("Example: 10,20,30,40,50,60\n") #Add the backslash and "n" to have space between the sections
 
         data_str = input("Enter your data here.")
-     #Create input area for data entry
-     #Printout the data in terminal
-    #The data provided always comes in string format hence the variable "data_str" used
+      #Create input area for data entry
+      #Printout the data in terminal
+      #The data provided always comes in string format hence the variable "data_str" used
 
-    #Validate the data provided
+      #Validate the data provided
 
         sales_data = data_str.split(",")
     
@@ -71,10 +72,42 @@ def update_sales_worksheet(data):
     sales_worksheet.append_row(data) #append_row adds a new row at the end of the sheet.
     print("Sales worksheet updated successfully.\n ")
 
+def calculate_surplus_data(sales_row):
+    """
+    Compare sales with stock and calculate the surplus for each item type.
 
-data = get_sales_data()
-sales_data = [int(num) for num in data] #We assign the result from the list comprehension to a new variable named sales_data
-update_sales_worksheet(sales_data)# we call the function n pass the sales_data _list
+    The surplus is defined as the sales figure subtracte from the stock:
+     -positive surplus inicates waste
+     -Negative surplus indicates extra made when stock was sold out.
+    """
+    print("Calculating surplus data...\n")
+    stock = SHEET.worksheet('stock').get_all_values() #This gets the last data of the stock sheet at tyhe bottom
+    pprint(stock) #Makes data easier to read wen printed to terminal,this is used instead of "print" but needs to be also imported at the top.
+    stock_row = stock[-1] #Accessing the last row of the spreadsheet
+    print(f"stock row: {stock_row}")
+    print(f"sales row: {sales_row}")
+    
+    surplus_data = [] #This is where we shall push the results from our calculation using the append method
+    for stock, sales in zip(stock_row, sales_row): #The zip method allows us to unpack the data.
+        surplus = int(stock) - sales #You have the int on the stock values to convert it to integers
+        surplus_data.append(surplus)
+
+    return surplus_data
+
+
+
+def main():
+    """
+    Run all program functions
+    """
+    data = get_sales_data()
+    sales_data = [int(num) for num in data] #We assign the result from the list comprehension to a new variable named sales_data
+    update_sales_worksheet(sales_data)# we call the function n pass the sales_data _list
+    new_surplus_data = calculate_surplus_data(sales_data)
+    print(new_surplus_data)
+
+print("Welcome to Love Sanwiches Data Automation") # So wehn you run the system,this message will be printed first before the others
+main()
 
 
 
