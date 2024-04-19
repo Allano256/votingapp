44f434591b95ac3bd844e80f7e3254ecb2266df6
allan zizinga <allanzizinga@yahoo.com>
2024-04-19 09:18:39 +0200

    add user request loop to repeat for data untill data provided is valid

--- run.py
+++ run.py
@@ -21,22 +21,27 @@ def get_sales_data():
     """ 
     Get sales figures input from the user
     """
+    while True:
+        print("Please enter sales data from the last market.")
+        print("data should be six numbers, separated by commas.")
+        print("Example: 10,20,30,40,50,60\n") #Add the backslash and "n" to have space between the sections
 
-    print("Please enter sales data from the last market.")
-    print("data should be six numbers, separated by commas.")
-    print("Example: 10,20,30,40,50,60\n") #Add the backslash and "n" to have space between the sections
-
-    data_str = input("Enter your data here.")
+        data_str = input("Enter your data here.")
      #Create input area for data entry
      #Printout the data in terminal
     #The data provided always comes in string format hence the variable "data_str" used
 
     #Validate the data provided
 
-    sales_data = data_str.split(",")
-    validate_data(sales_data)
+        sales_data = data_str.split(",")
+    
      # Use the split method to break it up at the commas,this will remove the commas from the string.
      #values is the sales data list in the parameter.
+
+        if validate_data(sales_data):
+            print("Data is valid")
+            break   #This validation part checks if the data enetered is true,and return True if it is and the while loop will end at the break key work,but if the wrong values are entered,the while loop will keep goin and ask for new data.
+    return sales_data
 
 def validate_data(values):
     """
@@ -53,8 +58,10 @@ def validate_data(values):
             )
     except ValueError as e:
         print(f"Invalid data: {e}, please try again.\n")
- 
+        return False #If there is an error in the data,so an error will be printed an the user asked to enter new data.
+    
+    return True #If no error is found when entering data,so this will also be the condition to end our while loop.
      
-get_sales_data()
+data = get_sales_data()
 
 # ['12','3','12','10','12','13']
