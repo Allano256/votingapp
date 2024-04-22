9d2d992b249c66a04eb937a3ab30c50ecc61a275
allan zizinga <allanzizinga@yahoo.com>
2024-04-22 16:21:18 +0200

    We have successfully created a validation that only numbers are accepted during voting

--- run.py
+++ run.py
@@ -17,20 +17,31 @@ SHEET = GSPREAD_CLIENT.open('voting_app')
 
 def voter_choice():
     """
-    This condition checks to see what the voter has chosen, either y,x,both or none.
+    This condition checks to see what the voter has chosen, either 1,2.
     """
     print("Please use either '1' symbol for candidate A or '2' for candidate B")
     print("Do not use both '1,2' to vote as this will be invalid vote")
     print("You are only allowed to vote once...\n")
-    choice_of_user = int(input("Please submit your vote here: "))
-    if choice_of_user == 1:
-        print(f"You voted for candidate A")
-    elif choice_of_user == 2:
-        print(f"You voted for candidate B ")
-    else:
-        print(f"{choice_of_user} entered is not a valid character...Please try again with either 1 or 2 as the options.")
+    
+    choice_user = (input("Please submit your vote here: "))
+    voter_choice = choice_user
    
+    validate_user_input(voter_choice)
+    
+   
+def validate_user_input(value):
+        """
+        This will check that only numbers are entered when selecting a candidate.
+         """
+        if value == 1:
+            print(f"You voted for candidate A")
+        elif value == 2:
+            print(f"You voted for candidate B ")
+        else:
+            print(f"{value} entered is not a valid character...Please try again with choices 1 and 2")
+
 voter_choice()
+    
 
 
 
