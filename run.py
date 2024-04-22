c04b19c35d75ac32bbc73004245c160dca324ed8
allan zizinga <allanzizinga@yahoo.com>
2024-04-22 14:50:47 +0200

    created the first function to get the users choice and then printout whio they chose,with an if else statement aswell

--- run.py
+++ run.py
@@ -8,13 +8,24 @@ SCOPE = [
     "https://www.googleapis.com/auth/drive"
     ]
 
-CREDS = Credentials.from_service_account_file('creds.json')
+CREDS = Credentials.from_service_account_file('votingapp.json')
 SCOPED_CREDS = CREDS.with_scopes(SCOPE)
 GSPREAD_CLIENT = gspread.authorize(SCOPED_CREDS)
 SHEET = GSPREAD_CLIENT.open('voting_app')
 
 
-candidates =SHEET.worksheet('candidates')
-data = candidates
 
+def voter_choice():
+    """
+    This condition checks to see what the voter has chosen, either y,x,both or none.
+    """
+    print("Please use either '1' symbol for candidate A or '2' for candidate B")
+    print("Do not use both '1,2' to vote as this will be invalid vote")
+    print("You are only allowed to vote once...")
+    choice = int(input("Please submit your vote here: "))
+    if choice == 1:
+        print(f"You voted for candidate A")
+    else:
+        print(f"You voted for candidate B ")
 
+voter_choice()
