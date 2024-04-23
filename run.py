2a749dfb8c21680171c95eaa74eae2b9af9883e4
allan zizinga <allanzizinga@yahoo.com>
2024-04-23 13:23:58 +0200

    created a while loop to allow the system to reload itself and request the user to make a new entry and only end when the right character is entered

--- run.py
+++ run.py
@@ -17,28 +17,36 @@ SHEET = GSPREAD_CLIENT.open('voting_app')
 
 def voter_choice():
     """
-    This condition checks to see what the voter has chosen, either 1,2.
+    This condition checks to see what the voter has chosen, either 1,2. Also ensure that the voter does not reload the page everytime they enter a wrong character but instead loads automatically using the while loop created.
     """
-    print("Please use either '1' symbol for candidate A or '2' for candidate B")
-    print("Do not use both '1,2' to vote as this will be invalid vote")
-    print("You are only allowed to vote once...\n")
+    while True:
+        print("Please use either '1' symbol for candidate A or '2' for candidate B")
+        print("Do not use both '1,2' to vote as this will be invalid vote")
+        print("You are only allowed to vote once...\n")
     
-    choice_user = (input("Please submit your vote here: "))
-    voter_choice = choice_user
+        choice_user = (input("Please submit your vote here: "))
+        voter_choice = choice_user
    
-    validate_user_input(voter_choice)
+       
+        if validate_user_input(voter_choice):
+            print("Congragulations,voting successful!")
+            break
+            
     
-   
 def validate_user_input(value):
-        """
-        This will check that only numbers are entered when selecting a candidate.
-         """
-        if value == 1:
-            print(f"You voted for candidate A")
-        elif value == 2:
-            print(f"You voted for candidate B ")
-        else:
-            print(f"{value} entered is not a valid character...Please try again with choices 1 and 2")
+    """
+    This will ensure that a voter only uses 1 or 2 to cast their vote, and also ensure that invalid characters are not allowed as entry into the voting system.
+    """
+    if value == "1":
+        print(f"You voted for candidate A")
+    elif value == "2":
+        print(f"You voted for candidate B")
+    else:
+        print(f"{value} chosen is not a valid character")
+        return False
+    return True
+   
+    
 
 voter_choice()
     
