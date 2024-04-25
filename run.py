ea539d6077b44690f767f40d102654862ca6a547
allan zizinga <allanzizinga@yahoo.com>
2024-04-25 12:56:35 +0200

    Add time.sleep and clear function

--- run.py
+++ run.py
@@ -1,3 +1,5 @@
+import os
+import time
 import gspread
 from google.oauth2.service_account import Credentials
 
@@ -8,12 +10,13 @@ SCOPE = [
     "https://www.googleapis.com/auth/drive"
     ]
 
-CREDS = Credentials.from_service_account_file('votingapp.json')
+CREDS = Credentials.from_service_account_file('creds.json')
 SCOPED_CREDS = CREDS.with_scopes(SCOPE)
 GSPREAD_CLIENT = gspread.authorize(SCOPED_CREDS)
 SHEET = GSPREAD_CLIENT.open('voting_app')
 
-
+def clear():
+    os.system("cls")
 
 def voter_choice():
     """
@@ -23,18 +26,25 @@ def voter_choice():
     print("Please use either A symbol for candidate A or B for candidate B")
     print("Do not use both 'A and B' to vote as this will be invalid vote")
     print("You are only allowed to vote once...\n")
+    time.sleep(2)
     
-    choice_user = (input("Please submit your vote here: "))
+    choice_user = input("Please submit your vote here: ").upper()
+    clear()
 
     global voter_choice 
     voter_choice = choice_user
+    time.sleep(1)
     
     if voter_choice == 'A' :
         print(f"You voted for candidate A")
+        input("press enter to continue")
+        clear()
         update_candidate_A_votes([1,0])
      
     elif voter_choice == 'B' :
         print(f"You voted for candidate B")
+        input("press enter to continue")
+        clear()
         update_candidate_A_votes([0,1])
        
     else:
@@ -51,17 +61,21 @@ def update_candidate_A_votes(voter_choice):
             """
 
     print("updating candidate A tally...\n")
+    time.sleep(2)
     candidate_worksheet = SHEET.worksheet('candidate')
     candidate_worksheet.append_row(voter_choice)
+    clear()
     print("Candidate A worksheet updated successfully \n")
     candidate_worksheet = SHEET.worksheet('candidate')
     candidate_data = candidate_worksheet.get_all_records()
-        
+    time.sleep(2)
+    clear()  
     print(candidate_data)
+    time.sleep(1)
     print('The voter choice is', voter_choice)
    
 
-
+clear()
 data = voter_choice()
 
 
